12c06175c2c24731fbb673583d7fe80bf593df80
arsalan1664 <119442314+arsalan1664@users.noreply.github.com>
2023-11-12 10:01:35 +0000

    depoly v1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
 import Blog from "./components/Blog";
 import Navbar from "./components/Navbar";
 import {
-  Routes,
-  Route,
-  useNavigate
+    Routes,
+    Route,
+    useNavigate
 } from 'react-router-dom'
 import { Register } from "./components/Register";
 import {
-  Login
+    Login
 } from "./components/Login";
 import MyBlog from "./components/MyBlog";
 import Footer from "./components/Footer";
@@ -22,46 +22,49 @@ interface ProtectedProps {
 export function Protected({ children }: ProtectedProps): ReactNode {
   const navigate = useNavigate();
   const userid = localStorage.getItem('id');
-  if (!userid) {
-    useEffect(() => navigate('/login'), [userid])
+  if(!userid){
+    useEffect(()=>navigate('/login'),[userid])
+    
   }
+
   return userid && (
     <>{children}</>
   )
 }
 
 export function ProtectedUser({ children }: ProtectedProps): ReactNode {
-  const navigate = useNavigate();
-  const userid = localStorage.getItem('id');
-  if (userid) {
-    useEffect(() => navigate('/'), [userid])
+    const navigate = useNavigate();
+    const userid = localStorage.getItem('id');
+    if(userid){
+      useEffect(()=>navigate('/'),[userid])
+    }
+    return !userid && (
+      <>{children}</>
+    )
   }
-  return !userid && (
-    <>{children}</>
-  )
-}
 
 
 function App() {
 
-  return (
-    <div className="relative h-screen w-screen bg-background text-foreground">
-      <Navbar />
-      <Routes>
-        <Route path='/login'
-          element={<ProtectedUser><Login /></ProtectedUser>} />
-        <Route path='/register'
-          element={<ProtectedUser><Register/></ProtectedUser>} />
-        <Route path='/'
-          element={<Blog />} />
-        <Route path='/my-blog'
-          element={<Protected><MyBlog /></Protected>} />
-        <Route path='/create-blog'
-          element={<Protected><CreateBlog /></Protected>} />
-      </Routes>
-      <Footer />
-    </div>
-  )
+    return (
+        <div className="relative h-screen w-screen bg-background text-foreground">
+            <Navbar />
+            <Routes>
+                <Route path='/login'
+                    element={<ProtectedUser><Login /></ProtectedUser>} />
+                <Route path='/register'
+                    element={<Register />} />
+                <Route path='/'
+                    element={<Blog />} />
+                <Route path='/my-blog'
+                    element={<Protected><MyBlog /></Protected>} />
+                <Route path='/create-blog'
+                    element={<Protected><CreateBlog /></Protected>} />
+            </Routes>
+            <Footer />
+            
+        </div>
+    )
 }
 
 export default App
